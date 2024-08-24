2d6689e54595ccfe1bc38a1b9ac7925e2fde3f63
Isabel Alberti <isabel.fagundes@sptech.school>
2024-08-24 13:16:52 -0300

    Adicionando telas do app

--- EstoqueToc/app/src/main/java/com/example/estoquetoc/MainActivity.kt
+++ EstoqueToc/app/src/main/java/com/example/estoquetoc/MainActivity.kt
@@ -3,39 +3,28 @@ package com.example.estoquetoc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
 import com.example.estoquetoc.ui.theme.EstoqueTocTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             EstoqueTocTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginScreen(modifier = Modifier.padding(innerPadding))
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    AppNavHost(navController = navController)
                 }
             }
         }
@@ -43,144 +32,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier) {
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var loginMessage by remember { mutableStateOf("") }
-
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(color = Color(0xFFEAAC47))
-            .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-    )
-    {
-        Box(
-            modifier = Modifier
-                .size(100.dp)
-                .align(Alignment.CenterHorizontally)
-                .background(
-                    Color.Transparent,
-                    shape = RoundedCornerShape(16.dp)
-                ),
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = "Logo",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .border(
-                    width = 1.dp,
-                    color = Color.LightGray,
-                    shape = RoundedCornerShape(10.dp)
-                )
-                .clip(RoundedCornerShape(16.dp)) // Garantir que o conteúdo interno siga o arredondamento
-                .background(Color.White)
-        ) {
-            TextField(
-                value = username,
-                onValueChange = { username = it },
-                colors = TextFieldDefaults.colors(
-                    focusedIndicatorColor = Color.Green,
-                ),
-                label = {
-                    Text("Digite seu e-mail")
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = Color.White
-                    )
-            )
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .border(
-                    width = 1.dp,
-                    color = Color.LightGray,
-                    shape = RoundedCornerShape(10.dp)
-                )
-                .clip(RoundedCornerShape(16.dp)) // Garantir que o conteúdo interno siga o arredondamento
-                .background(Color.White)
-        ) {
-            TextField(
-                value = password,
-                onValueChange = { password = it },
-                label = { Text("Digite sua senha") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = Color.White
-                    ),
-                visualTransformation = PasswordVisualTransformation()
-            )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = {
-                if (username == "admin" && password == "1234") {
-                    loginMessage = "Login realizado"
-                } else {
-                    loginMessage = "E-mail ou senha inválido(s)"
-                }
-            },
-            modifier = Modifier
-                .width(150.dp)
-                .align(Alignment.CenterHorizontally)
-        ) {
-            Text("Entrar")
-        }
-
-        Spacer(modifier = Modifier.height(4.dp))
-        ForgotPasswordLink()
-
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = loginMessage)
+fun AppNavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") { LoginScreen(navController) }
+        composable("cadastro_usuario") { CadastroUsuarioScreen(navController) }
+        composable("dashboard") { DashboardScreen(navController) }
+        composable("cadastro_produto") { CadastroProdutoScreen(navController) }
+        composable("cadastro_fornecedor") { CadastroFornecedorScreen(navController) }
+        composable("cadastro_funcionario") { CadastroFuncionarioScreen(navController) }
     }
-}
-
-@Composable
-fun ForgotPasswordLink() {
-    var isHovered by remember { mutableStateOf(false) }
-
-    Text(
-        text = "Esqueci a senha",
-        color = Color.Blue,
-        style = TextStyle(
-            fontWeight = FontWeight.Light,
-            textDecoration = if (isHovered) TextDecoration.Underline else TextDecoration.None
-        ),
-        modifier = Modifier
-            .clickable {
-                // redireciona para tela de cadastro
-            }
-            .padding(8.dp)
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onPress = {
-                        isHovered = true
-                        tryAwaitRelease()
-                        isHovered = false
-                    }
-                )
-            }
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun LoginScreenPreview() {
+fun DefaultPreview() {
     EstoqueTocTheme {
-        LoginScreen()
+        AppNavHost(navController = rememberNavController())
     }
 }
