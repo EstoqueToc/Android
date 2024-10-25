08421a8b4a16b79401e6aeade2c5a8dedc08915f
Lúcio Garcia Rios <lucio.rios@sptech.school>
2024-10-25 10:36:48 -0300

    adicionando leitura codigo de barras no cadastro de produto

--- EstoqueToc/app/build.gradle.kts
+++ EstoqueToc/app/build.gradle.kts
@@ -56,6 +56,15 @@ android {
 
 dependencies {
 
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+    implementation("com.google.zxing:core:3.5.0")
+
+    implementation("androidx.compose.ui:ui:1.5.0")
+    implementation("androidx.compose.material3:material3:1.1.0")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
     implementation ("androidx.compose.ui:ui:1.5.0")
     implementation ("androidx.compose.material3:material3:1.1.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
@@ -87,6 +96,8 @@ dependencies {
 
     // WebView
     implementation (libs.androidx.webkit.v140)
+
+
 
     // Testing dependencies
     testImplementation(libs.junit)
---
+++ EstoqueToc/app/src/main/java/com/example/estoquetoc/codigoDeBarra/CustomCaptureActivity.kt
@@ -0,0 +1,5 @@
+package com.example.estoquetoc.codigoDeBarra
+
+import com.journeyapps.barcodescanner.CaptureActivity
+
+class CustomCaptureActivity : CaptureActivity()
